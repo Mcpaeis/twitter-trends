de26b5aebf15dc18ac469eb7d7d5a8c843a09ef1
Philip Lin <129629958+therealdoglover@users.noreply.github.com>
2023-12-11 21:11:31 -0600

    Update Shiny LDA.R

--- LDA/Shiny LDA.R
+++ LDA/Shiny LDA.R
@@ -2,11 +2,23 @@ library(shiny)
 library(ggplot2)
 library(readr)
 
+url <- "https://github.com/Mcpaeis/twitter-trends/raw/master/data/tweets_data_v2.csv.zip"
+download.file(url, destfile = "tweets_data_v2.zip")
+unzip("tweets_data_v2.zip", exdir = "data")
+tweets_data <- read.csv("data/tweets_data_v2.csv")
+
+# Extract unique county names
+unique_counties <- tweets_data %>%
+  distinct(county) %>%
+  pull(county)
+
 ui <- fluidPage(
   titlePanel("LDA Output Visualization"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("state", "Select State", choices = c("All", "Alabama", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Luisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wyoming", "Wisconsin"))
+      selectInput("choice", "Select Mode", choices = c("All", "Statewise", "Countywise")),
+      uiOutput("stateSelector"),
+      uiOutput("countySelector")
     ),
     mainPanel(
       plotOutput("ldaPlot")
@@ -14,13 +26,34 @@ ui <- fluidPage(
   )
 )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+  # Dynamic UI for state selection
+  output$stateSelector <- renderUI({
+    if (input$choice == "Statewise") {
+      selectInput("state", "Select State", choices = c("Alabama", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Luisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wyoming", "Wisconsin"))
+    }
+  })
+  
+  # Dynamic UI for county selection
+  output$countySelector <- renderUI({
+    if (input$choice == "Countywise") {
+      selectInput("county", "Select County", choices = unique_counties)
+    }
+  })
   
   output$ldaPlot <- renderPlot({
-    if (input$state == "All") {
+    data_path <- ""
+    
+    if (input$choice == "All") {
       data_path <- "https://raw.githubusercontent.com/Mcpaeis/twitter-trends/master/LDA/top_terms_df_all.csv"
-    } else {
-      data_path <- paste0("https://raw.githubusercontent.com/Mcpaeis/twitter-trends/master/LDA/", input$state, "_lda.csv")
+    } else if (input$choice == "Statewise") {
+      # Replace spaces with '%20' for the URL
+      state_name <- gsub(" ", "%20", input$state)
+      data_path <- paste0("https://raw.githubusercontent.com/Mcpaeis/twitter-trends/master/LDA/", state_name, "_lda.csv")
+    } else if (input$choice == "Countywise") {
+      # Replace spaces with '%20' for the URL
+      county_name <- gsub(" ", "%20", input$county)
+      data_path <- paste0("https://raw.githubusercontent.com/Mcpaeis/twitter-trends/master/LDA/County/", county_name, "_lda.csv")
     }
     
     top_terms <- read_csv(data_path)
@@ -30,10 +63,10 @@ server <- function(input, output) {
       geom_bar(stat = "identity") +
       coord_flip() +
       facet_wrap(~ topic, scales = "free") +
-      scale_fill_viridis_d() +  # This line adds a color scale
-      labs(x = "Term", y = "Probability", title = paste("Top 10 Words in Each Topic -", input$state)) +
+      scale_fill_viridis_d() +
+      labs(x = "Term", y = "Probability", title = paste("Top 10 Words in Each Topic -", ifelse(input$choice == "All", "All", ifelse(input$choice == "Statewise", input$state, paste(input$county))))) +
       theme_minimal() +
-      theme(legend.position = "none")  # This line removes the legend
+      theme(legend.position = "none")
   })
 }
 
